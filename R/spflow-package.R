4c6a7f42c26905464b77a9e0a44b4dc87c5fa804
Luke <lukece@mailbox.org>
2020-04-29 11:31:56 +0200

    Add package level documentation

---
+++ R/spflow-package.R
@@ -0,0 +1,22 @@
+#' spflow: A package for spatial econometric interaction models.
+#'
+#' The foo package provides function to estimate origin destination flows with
+#' that exhibit spatial autocorrelation.
+#'
+#'
+#' @section Network data:
+#' The package provides a new class for representing network data.
+#'
+#' @section Model estimation:
+#' The package provides functions to estimate spatial interaction models.
+#'
+#' @docType package
+#' @import methods Matrix
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
